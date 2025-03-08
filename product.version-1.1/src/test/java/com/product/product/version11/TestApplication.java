f805a244ca3470005dcc4624ab90893110b288f1
Balaji <balajidaggumilli67944@gmail.com>
2025-03-08 17:17:03 +0530

    "Product Service has built"

---
+++ product.version-1.1/src/test/java/com/product/product/version11/TestApplication.java
@@ -0,0 +1,11 @@
+package com.product.product.version11;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
