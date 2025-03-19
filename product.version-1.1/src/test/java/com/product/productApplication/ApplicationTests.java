3cf0b99a6a9af60cb738f743af649dcd25aa9e61
Balaji <balajidaggumilli67944@gmail.com>
2025-03-19 21:41:23 +0530

    done product service but need to integration test

---
+++ product.version-1.1/src/test/java/com/product/productApplication/ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.product.productApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product.version-1.1/src/test/java/com/product/productApplication/TestApplication.java
@@ -0,0 +1,12 @@
+package com.product.productApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
